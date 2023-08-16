7cb2958a36898cd083e88c76d89ae4e8bdbd4b51
Doug Turnbull <softwaredoug@gmail.com>
2023-08-16 19:49:59 -0400

    Bad attempt at numpy extension

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""setup.py file for single_type_logit.c.
+
+Note that since this is a numpy extension
+we use numpy.distutils instead of
+distutils from the python standard library.
+
+Calling
+$python setup.py build_ext --inplace
+will build the extension library in the npufunc_directory.
+
+Calling
+$python setup.py build
+will build a file that looks like ./build/lib*, where
+lib* is a file that begins with lib. The library will
+be in this file and end with a C library extension,
+such as .so
+
+Calling
+$python setup.py install
+will install the module in your site-packages file.
+
+See the distutils section of
+'Extending and Embedding the Python Interpreter'
+at docs.python.org  and the documentation
+on numpy.distutils for more information.
+"""
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('npufunc_directory',
+                           parent_package,
+                           top_path)
+    config.add_extension('npufunc', ['ufunc.c'])
+
+    return config
+
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(configuration=configuration)
---
+++ ufunc.c
@@ -0,0 +1,95 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include "numpy/ndarraytypes.h"
+#include "numpy/ufuncobject.h"
+#include "numpy/halffloat.h"
+#include <math.h>
+
+/*
+ * multi_arg_logit.c
+ * This is the C code for creating your own
+ * NumPy ufunc for a multiple argument, multiple
+ * return value ufunc. The places where the
+ * ufunc computation is carried out are marked
+ * with comments.
+ *
+ * Details explaining the Python-C API can be found under
+ * 'Extending and Embedding' and 'Python/C API' at
+ * docs.python.org.
+ */
+
+static PyMethodDef LogitMethods[] = {
+    {NULL, NULL, 0, NULL}
+};
+
+/* The loop definition must precede the PyMODINIT_FUNC. */
+
+static void unshared_bits(char **args, const npy_intp *dimensions,
+                          const npy_intp *steps, void *data)
+{
+    npy_intp i;
+    npy_intp n = dimensions[0];
+    char *in1 = args[0], *in2 = args[1];
+    char *out1 = args[2];
+    npy_intp in1_step = steps[0], in2_step = steps[1];
+    npy_intp out1_step = steps[2];
+
+    double tmp;
+
+    for (i = 0; i < n; i++) {
+        /* BEGIN main ufunc computation */
+        tmp = *(double *)in1;
+        tmp *= *(double *)in2;
+        *((double *)out1) = tmp;
+        // *((double *)out2) = log(tmp / (1 - tmp));
+        /* END main ufunc computation */
+
+        in1 += in1_step;
+        in2 += in2_step;
+        out1 += out1_step;
+    }
+}
+
+/*This a pointer to the above function*/
+PyUFuncGenericFunction funcs[1] = {&unshared_bits};
+
+/* These are the input and return dtypes of logit.*/
+
+static char types[3] = {NPY_UINT64, NPY_UINT64,
+                        NPY_UINT8};
+
+static struct PyModuleDef moduledef = {
+    PyModuleDef_HEAD_INIT,
+    "npufunc",
+    NULL,
+    -1,
+    LogitMethods,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+PyMODINIT_FUNC PyInit_npufunc(void)
+{
+    PyObject *m, *num_unshared, *d;
+
+    import_array();
+    import_umath();
+
+    m = PyModule_Create(&moduledef);
+    if (!m) {
+        return NULL;
+    }
+
+    num_unshared = PyUFunc_FromFuncAndData(funcs, NULL, types, 3, 2, 1,
+                                           PyUFunc_None, "num_unshared_bits",
+                                           "num_unshared_bits_dicstr", 0);
+
+    d = PyModule_GetDict(m);
+
+    PyDict_SetItemString(d, "num_unshared_bits", num_unshared);
+    Py_DECREF(num_unshared);
+
+    return m;
+}
