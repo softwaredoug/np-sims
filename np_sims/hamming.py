992a9b5db3178c9a118afd892d66eac163f18996
Doug Turnbull <softwaredoug@gmail.com>
2023-08-19 10:09:55 -0400

    Test hamming separately

--- np_sims/hamming.py
+++ np_sims/hamming.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# Save masks to avoid recomputing
 # Naive bitcount implementation without popcount
 
 m1 = np.uint64(0x5555555555555555)
@@ -18,20 +19,14 @@ num_bytes_64 = 8
 
 
 def bit_count64(arr: np.ndarray[np.uint64]) -> np.ndarray:
-    #
-    # Taken from Mad Physicist on Stackoverflow:
-    #   https://stackoverflow.com/a/68943135/8123
-    #
-    # Make the values type-agnostic (as long as it's integers)
-    # baseline
-    #
-    # arr = arr - ((arr >> 1) & s55)
-    # arr = (arr & s33) + ((arr >> 2) & s33)
-    # arr = (arr + (arr >> 4)) & s0F
-    # return (arr * s01) >> (8 * (arr.itemsize - 1))
+    """Count bits per element in a 64 bit array.
 
+    Adapted from Mad Physicist's answer on Stackoverflow:
+          https://stackoverflow.com/a/68943135/8123
+
+    """
     # Inplace, (ie -=) probably fastesr
-    arr = arr - ((arr >> 1) & s55)
+    arr -= ((arr >> 1) & s55)
     arr = (arr & s33) + ((arr >> 2) & s33)
 
     # Using inplace operators where possible
@@ -41,3 +36,26 @@ def bit_count64(arr: np.ndarray[np.uint64]) -> np.ndarray:
     arr >>= (8 * (num_bytes_64 - 1))
 
     return arr
+
+
+def hamming_naive(hashes, query, count_function=bit_count64):
+    """Compute an inverse hamming similarity score. 0 is identical, query.shape[1] * 64 is completely different.
+
+    Returns
+    -------
+    array with each hash's similarity to query, for each row
+     - 0 if identical to query, query.shape[1] * 64 if completely different
+
+    normalize to 0-1 by dividing by query.shape[1] / 64
+
+    Parameters
+    ----------
+    hashes are many bit arrays of LSH hashes corresponding to the vectors we've indexed
+    query is a bit array of LSH hashee with the vector we've queried
+
+    """
+    # Note there are copies here, if you XOR'd in place, you might save some performance as then you
+    # don't copy the giant hash table...
+    #
+    # but you also have to undo that XOR
+    return np.sum(count_function(hashes ^ query), axis=1)
---
+++ test/test_hamming.py
@@ -0,0 +1,31 @@
+import pytest
+import numpy as np
+from np_sims.hamming import bit_count64, hamming_naive
+
+
+bit_tests = [
+    ([0b0010], np.uint64(1)),
+    ([0b0111], np.uint64(3)),
+    ([np.iinfo(np.uint64).max], np.uint64(64)),
+    ([np.iinfo(np.uint64).min], np.uint64(0)),
+    ([np.iinfo(np.uint64).max, np.iinfo(np.uint64).max], [np.uint64(64), np.uint64(64)]),
+]
+
+
+@pytest.mark.parametrize("bit_array, expected", bit_tests)
+def test_naive_bitcount(bit_array, expected):
+    assert (bit_count64(np.array(bit_array, dtype=np.uint64)) == expected).all()
+
+
+# Given hashes (first param) compute number of DISSIMILAR bits
+hamming_tests = [
+    ([[0b0010]],           [0b0010], np.uint64(0)),  # noqa: E241
+    ([[0b0010], [0b1101]], [0b0010], [np.uint64(0), np.uint64(4)]),
+    ([[0], [np.iinfo(np.uint64).max]], [0], [np.uint64(0), np.uint64(64)]),
+]
+
+
+@pytest.mark.parametrize("hashes, query, expected", hamming_tests)
+def test_hamming(hashes, query, expected):
+    assert (hamming_naive(np.array(hashes, dtype=np.uint64),
+                          np.array(query, dtype=np.uint64)) == expected).all()
