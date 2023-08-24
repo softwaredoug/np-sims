ec003bd880103d39f6c2387beb4f6844959d8415
Doug Turnbull <softwaredoug@gmail.com>
2023-08-24 12:40:37 -0400

    Test deeper tree

--- test/test_rptrees.py
+++ test/test_rptrees.py
@@ -7,22 +7,20 @@ from np_sims.rp_trees import fit, rp_hash
 
 
 def as_normed_vects(vectors):
-    return vectors / np.linalg.norm(vectors, axis=1)
-
-
-def str_to_bits(s):
-    bits = np.uint64(0)
-    for idx, curr_char in enumerate(s):
-        if curr_char == "1":
-            bits |= np.uint64(1) << (len(s) - idx - 1)
-    return np.array([bits])
+    return vectors / np.linalg.norm(vectors, axis=1)[:, None]
 
 
 vector_test_scenarios = {
     "base": {
         "vectors": as_normed_vects(np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])),
         "query": as_normed_vects(np.array([[0, 1, 2]])),
-        "expected_query_hash": str_to_bits("000"),
+        "expected_match": 0
+    },
+    "large": {
+        "vectors": as_normed_vects(np.array([[0, 1.1, 2], [3, 4.1, 5], [6, 7.1, 8],
+                                             [0, 1.5, 2], [3, 4.5, 5], [6, 7.5, 8]])),
+        "query": as_normed_vects(np.array([[0, 1, 2]])),
+        "expected_match": 0
     }
 }
 
@@ -36,7 +34,7 @@ def w_scenarios(scenarios: Dict[str, Dict[str, Any]]):
 
 
 @w_scenarios(vector_test_scenarios)
-def test_tree_building(vectors, query, expected_query_hash):
-    tree = fit(vectors, depth=3)
-    vectors_hashed = rp_hash(tree, vectors, depth=3)
+def test_tree_building(vectors, query, expected_match):
+    tree = fit(vectors, depth=10)
+    vectors_hashed = rp_hash(tree, vectors, depth=10)
     assert len(set(vectors_hashed)) == vectors_hashed.shape[0]
