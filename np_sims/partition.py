374c2a0a6ed594b617456ec34f7853370e5f044a
Doug Turnbull <softwaredoug@gmail.com>
2023-08-26 16:50:49 -0400

    Move partition between to splitting strategy

--- np_sims/partition.py
+++ np_sims/partition.py
@@ -12,6 +12,7 @@ As that idiot Farts McToots says: "It's a tree, but it's made of vectors."
 """
 
 import numpy as np
+from np_sims.lsh import random_projection
 
 
 class SplitRule:
@@ -41,3 +42,131 @@ def kdtree_chooserule(vectors: np.ndarray) -> SplitRule:
     rand_dim = np.random.randint(0, vectors.shape[1])
     median = np.median(vectors[:, rand_dim])
     return KdTreeSplitRule(median, rand_dim)
+
+
+def all_but(vect, idx):
+    if idx < 0:
+        raise ValueError("Only indexes > 0 supported")
+    if idx == len(vect):
+        excluded = vect[:-1]
+    else:
+        excluded = np.concatenate([vect[:idx], vect[idx + 1:]])
+    assert len(excluded) == len(vect) - 1
+    return excluded
+
+
+# You can find a projection (p1...p2) that gives the opposite
+# signed dot products by solving the inequalities
+#
+# a1 * p1 + a2 * p2 ... + an * pn >= 0
+# b1 * p1 + b2 * b2 ... + bn * pn < 0
+#
+# --------
+# Subtract one of the dot product components
+#
+# a1 * p1 + a2 * p2 ... >= -(an * pn)
+# b1 * p1 + b2 * b2 ... <  -(bn * pn)
+#
+# -------
+# Multiply both sides by -1
+#
+# -(a1 * p1 + a2 * p2 ...) <= an * pn
+# -(b1 * p1 + b2 * b2 ...) >  bn * pn
+#
+# --------
+# Rearrange
+#
+# an * pn >= -(a1 * p1 + a2 * p2 + ... )
+# bn * pn <  -(b1 * p1 + b2 * p2 + ... )
+#
+# BOTH POSITIVE / BOTH NEGATIVE
+# -----------------------------
+# If an, bn are positive, we simply divide by an and bn
+#
+# pn >= -(a1 * p1 + a2 * p2 + ... ) / an
+# pn <  -(b1 * p1 + b2 * p2 + ... ) / bn
+#
+# If both are negative, we flip b and a, and we get a similar
+# inequality. Regardless one of the inequalities is the floor,
+# the other is the ceiling, and pn can be anything in between.
+#
+# -----------
+# IF an IS NEGATIVE, BUT bn IS POSITIVE
+#
+# an is negative sign flips for 'a' inequality
+# pn < -(a1 * p1 + a2 * p2 + ... ) / an
+# but b remains...
+# pn < -(b1 * p1 + b2 * p2 + ... ) / bn
+#
+# Since vectors a and b are normalized, pn could just be -1
+# with p1..pn-1 as 0s
+#
+# -----------
+# If an POSITIVE, BUT bn IS NEGATIVE
+#
+# Same as before, except pn=1
+#
+# Since these are normalized vectors, set pn to -1 and normarize
+def projection_between(vect1: np.ndarray,
+                       vect2: np.ndarray,
+                       dim=0):
+    """Get a vector that gives opposite signed dot products for vect1/vect2."""
+    if np.isclose(vect1, vect2).all():
+        raise ValueError("Vectors cannot be near identical")
+    dims = len(vect1)
+
+    while vect1[dim] == 0.0 or vect2[dim] == 0.0:
+        dim += 1
+
+    # To get the floor / ceiling we solve an inequality
+    # whose sign flips depending on whether vectn[dim] is
+    # positive or negative
+    if np.sign(vect1[dim]) == np.sign(vect2[dim]):
+        projection = random_projection(dims)
+
+        dot1 = np.dot(all_but(projection, dim),
+                      all_but(vect1, dim))
+        dot2 = np.dot(all_but(projection, dim),
+                      all_but(vect2, dim))
+
+        floor = -dot1 / vect1[dim]
+        ceiling = -dot2 / vect2[dim]
+        projection[dim] = (floor + ceiling) / 2.0
+
+    # Sign is different we just set to -1 or 1 which is basically
+    # like bisecting by this axis (like in 2 dims, this would be
+    # the y axis)
+    else:
+        projection = np.zeros(vect1.shape)
+        projection[dim] = np.sign(vect1[dim])
+
+    projection /= np.linalg.norm(projection)
+    return projection
+
+
+class ProjectionBetweenSplitRule(SplitRule):
+
+    def __init__(self, projection: np.ndarray):
+        self.projection = projection
+
+    def split(self, vectors: np.ndarray):
+        """Split vectors by the projection."""
+        left = np.ravel(np.argwhere(np.dot(vectors, self.projection) < 0))
+        right = np.ravel(np.argwhere(np.dot(vectors, self.projection) >= 0))
+        return left, right
+
+
+def projection_between_chooserule(vectors: np.ndarray, max_proximity=0.95) -> SplitRule:
+    """Choose a random projection vector that splits two random vectors chosen."""
+    # Choose two random vectors
+    vect1, vect2 = vectors[np.random.choice(len(vectors), 2, replace=False)]
+
+    tries = 0
+    while np.dot(vect1, vect2) > max_proximity:
+        vect1, vect2 = vectors[np.random.choice(len(vectors), 2, replace=False)]
+        tries += 1
+        if tries > 10:
+            return kdtree_chooserule(vectors)
+
+    proj = projection_between(vect1, vect2)
+    return ProjectionBetweenSplitRule(proj)
--- test/test_partition.py
+++ test/test_partition.py
@@ -2,7 +2,8 @@
 import numpy as np
 
 from test_utils import as_normed_vects, w_scenarios, many_close_to
-from np_sims.partition import kdtree_chooserule
+from np_sims.partition import kdtree_chooserule, projection_between_chooserule
+from np_sims.partition import KdTreeSplitRule, ProjectionBetweenSplitRule
 
 
 vector_test_scenarios = {
@@ -19,6 +20,10 @@ vector_test_scenarios = {
         "vectors": as_normed_vects(np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]]
                                             + many_close_to([3.0, 4.0, 5.0]))),
         "rng_seed": 100
+    },
+    "all_close": {
+        "vectors": as_normed_vects(many_close_to([3.0, 4.0, 5.0])),
+        "rng_seed": 1289519
     }
 }
 
@@ -37,3 +42,30 @@ def test_partition_kdtree(vectors, rng_seed):
 
     assert (left_vectors[:, splitter.dim] < splitter.median).all()
     assert (right_vectors[:, splitter.dim] >= splitter.median).all()
+
+
+@w_scenarios(vector_test_scenarios)
+def test_partition_proj_between(vectors, rng_seed):
+    np.random.seed(rng_seed)
+
+    max_proximity = 0.95
+    splitter = projection_between_chooserule(vectors, max_proximity)
+    left, right = splitter.split(vectors)
+    assert len(left) != 0
+    assert len(right) != 0
+    assert len(left) + len(right) == len(vectors)
+    left_vectors = vectors[left]
+    right_vectors = vectors[right]
+
+    if isinstance(splitter, KdTreeSplitRule):
+        assert (left_vectors[:, splitter.dim] < splitter.median).all()
+        assert (right_vectors[:, splitter.dim] >= splitter.median).all()
+        # This should be true when we get a kdtree back
+        # Dot product vectors together
+        # dotted = np.dot(vectors, vectors.T)
+        # assert (dotted >= max_proximity).all()
+    elif isinstance(splitter, ProjectionBetweenSplitRule):
+        assert (np.dot(left_vectors, splitter.projection) < 0).all()
+        assert (np.dot(right_vectors, splitter.projection) >= 0).all()
+    else:
+        raise AssertionError("Expected either KdTreeSplitRule or ProjectionBetweenSplitRule")
